a8270600d999235256f97f931d4b4eb60a1097e7
Gabriel Gregório da Silva <48621655+gabriel-gregorio-da-silva@users.noreply.github.com>
2019-05-31 16:26:30 -0300

    Add files via upload

---
+++ Colisão e gravidade no Csharp/Colisão e gravidade no Csharp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Colisão_e_gravidade_no_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.EU_SOU_O_TEMPO = new System.Windows.Forms.Timer(this.components);
+            this.BTN_CAIXA = new System.Windows.Forms.Button();
+            this.BTN_CHAO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // EU_SOU_O_TEMPO
+            // 
+            this.EU_SOU_O_TEMPO.Enabled = true;
+            this.EU_SOU_O_TEMPO.Interval = 1;
+            this.EU_SOU_O_TEMPO.Tick += new System.EventHandler(this.EU_SOU_O_TEMPO_Tick_1);
+            // 
+            // BTN_CAIXA
+            // 
+            this.BTN_CAIXA.BackColor = System.Drawing.Color.Transparent;
+            this.BTN_CAIXA.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTN_CAIXA.BackgroundImage")));
+            this.BTN_CAIXA.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTN_CAIXA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_CAIXA.ForeColor = System.Drawing.Color.Transparent;
+            this.BTN_CAIXA.Location = new System.Drawing.Point(220, 12);
+            this.BTN_CAIXA.Margin = new System.Windows.Forms.Padding(0);
+            this.BTN_CAIXA.Name = "BTN_CAIXA";
+            this.BTN_CAIXA.Size = new System.Drawing.Size(109, 101);
+            this.BTN_CAIXA.TabIndex = 0;
+            this.BTN_CAIXA.UseVisualStyleBackColor = false;
+            // 
+            // BTN_CHAO
+            // 
+            this.BTN_CHAO.BackColor = System.Drawing.Color.Transparent;
+            this.BTN_CHAO.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTN_CHAO.BackgroundImage")));
+            this.BTN_CHAO.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTN_CHAO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_CHAO.ForeColor = System.Drawing.Color.Transparent;
+            this.BTN_CHAO.Location = new System.Drawing.Point(52, 559);
+            this.BTN_CHAO.Margin = new System.Windows.Forms.Padding(0);
+            this.BTN_CHAO.Name = "BTN_CHAO";
+            this.BTN_CHAO.Size = new System.Drawing.Size(456, 54);
+            this.BTN_CHAO.TabIndex = 1;
+            this.BTN_CHAO.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 631);
+            this.Controls.Add(this.BTN_CHAO);
+            this.Controls.Add(this.BTN_CAIXA);
+            this.Name = "Form1";
+            this.Text = "Gravidade e Colisão no Csharp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer EU_SOU_O_TEMPO;
+        private System.Windows.Forms.Button BTN_CAIXA;
+        private System.Windows.Forms.Button BTN_CHAO;
+    }
+}
+
---
+++ Colisão e gravidade no Csharp/Colisão e gravidade no Csharp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colisão_e_gravidade_no_Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        /* Gravidade e colisão no form do C#. Versão 1.2
+        Autor: Gabriel Gregório da Silva
+        E-mail: gabriel.gregorio.1@outlook.com
+        Créditos: Não roube meus créditos hahahah
+
+        Como Implementar? Cole esse código no formulário
+        do seu game e siga as intruções abaixo
+            _______________________________________________________
+        |   COMO USAR O MÉTODO DA GRAVIDADE?                    |
+        |   gravidade(caixa,chao);                              |
+        |                                                       |
+        |  Exemplo para fornecer a gravidade para o             |
+        |  botão "caixa", Até tocar no Botão "chao":             |
+        |  gravidade(caixa , chao);                             |
+        |_______________________________________________________|
+        _______________________________________________________
+        |  COMO USAR O MÉTODO DE COLISÃO?                       |
+        |  colisao(objeto_1, objeto_2);                         |
+        |                                                       |
+        |  Exemplo para verificar se o button1 toca no button2: |
+        |  if ( colisao(button1 , button2) == "sim")            |
+        |  {                                                    |
+        |      // button1 tocou no button2, ou vice versa!      |
+        |  };                                                   |
+        |_______________________________________________________|
+        */
+
+        // *************** CALCULOS DE GRAVIDADE *************** \\
+        private void gravidade(Button objeto, Button chao_referencia)
+        {
+            int x = var_global.tempo;    // Tempo decorrido!.
+            int g = 10;                  // Gravidade relativa.
+            int vi = 0;                  // Velocidade Inicial.
+            int fator_redução = 300;     // Reduz a Intensidade da gravidade. (Padrão é 300, caso você mude, terá que atualizar alguns dados no método gravidade)
+
+            /* Vamos chamar o método de colisão para verificar se
+                o objeto que está sofrendo a ação da gravidade já
+                tocou no 'chão'. Caso verdade, vamos 'parar' a gravidade*/
+            if (colisao(objeto, chao_referencia) == "sim")
+            {
+                var_global.tempo = 1;    // O objeto já tocou no chão
+            }
+            else
+            {
+                // Queda no eixo Y
+                int y = Convert.ToInt16((-(g * x ^ 2) / (2 * vi ^ 2)) / fator_redução);
+
+                // Atualização da posição do objeto
+                objeto.Location = new Point(objeto.Location.X, objeto.Location.Y - y);
+
+                // Atualização da Variável Global
+                var_global.tempo = var_global.tempo + 1;
+            }
+        }
+
+        // *************** CALCULOS DE COLISÃO *************** \\
+        public string colisao(Button objeto_1, Button objeto_2)
+        {
+            string colidiu = "nada"; // Retorna se houve uma colisão ou não
+            Button c;                // Ajuda na inversão para multiplos testes
+            int x = 1;               // Fazer os mesmos testes em elementos diferentes
+
+            // Testar os dois lados
+            while (x < 3)
+            {
+                if (objeto_1.Location.X < objeto_2.Location.X && objeto_1.Location.X + objeto_1.Size.Width > objeto_2.Location.X && objeto_1.Location.Y < objeto_2.Location.Y && objeto_1.Location.Y + objeto_1.Size.Height > objeto_2.Location.Y)
+                {
+                    colidiu = "sim";
+                }
+                // Inversão de botões
+                c = objeto_1;
+                objeto_1 = objeto_2;
+                objeto_2 = c;
+
+                // Andante pelo loop
+                x = x + 1;
+            }
+
+            x = 1;
+            // Testar os dois lados
+            while (x < 3)
+            {
+                if (objeto_2.Location.X + objeto_2.Width < objeto_1.Location.X + objeto_1.Width && objeto_2.Location.X + objeto_2.Width > objeto_1.Location.X && objeto_2.Location.Y + objeto_2.Height < objeto_1.Location.Y + objeto_1.Height && objeto_2.Location.Y + objeto_2.Height > objeto_1.Location.Y)
+                {
+                    colidiu = "sim";
+                }
+                // Inversão de botões
+                c = objeto_1;
+                objeto_1 = objeto_2;
+                objeto_2 = c;
+
+                // Andante pelo loop
+                x = x + 1;
+            }
+
+            x = 1;
+            // Testar os dois lados
+            while (x < 3)
+            {
+                if (objeto_2.Location.X + objeto_2.Width < objeto_1.Location.X + objeto_1.Width && objeto_2.Location.X + objeto_2.Width > objeto_1.Location.X && objeto_2.Location.Y < objeto_1.Location.Y + objeto_1.Height && objeto_2.Location.Y > objeto_1.Location.Y)
+                {
+                    colidiu = "sim";
+                }
+                // Inversão de botões
+                c = objeto_1;
+                objeto_1 = objeto_2;
+                objeto_2 = c;
+
+                // Andante pelo loop
+                x = x + 1;
+            }
+            x = 1;
+
+            // Testar os dois lados
+            while (x < 3)
+            {
+                if (objeto_2.Location.X < objeto_1.Location.X + objeto_1.Width && objeto_2.Location.X > objeto_1.Location.X && objeto_2.Location.Y < objeto_1.Location.Y + objeto_1.Height && objeto_2.Location.Y > objeto_1.Location.Y)
+                {
+                    colidiu = "sim";
+                }
+                // Inversão de botões
+                c = objeto_1;
+                objeto_1 = objeto_2;
+                objeto_2 = c;
+
+                // Andante pelo loop
+                x = x + 1;
+            }
+
+            return (colidiu);
+        }
+
+        // Lista de variáveis Globais
+        static class var_global
+        {
+            public static int tempo = 1;
+        }
+
+        // A Cada Clock do relógio (Tempo do jogo)
+        private void EU_SOU_O_TEMPO_Tick_1(object sender, EventArgs e)
+        {
+            // Vamos chamar a "gravidade"
+            gravidade(BTN_CAIXA, BTN_CHAO);
+        }
+    }
+}
